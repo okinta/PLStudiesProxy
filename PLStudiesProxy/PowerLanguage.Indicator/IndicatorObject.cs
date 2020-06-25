29210ea5466fa1b3c4ed017b05df325e4a27f0cd
Robert Roose <robert@shivercube.com>
2020-06-25 15:31:50 +0100

    Implemented IndicatorObject class

---
+++ PLStudiesProxy/PowerLanguage.Indicator/IndicatorObject.cs
@@ -0,0 +1,13 @@
+﻿namespace PowerLanguage.Indicator
+{
+    /// <summary>
+    /// Indicator’s base class (inherit from this class to create an indicator).
+    /// </summary>
+    public abstract class IndicatorObject : CStudyAbstract
+    {
+        /// <summary>
+        /// Initializes a new instance of the IndicatorObject class.
+        /// </summary>
+        protected IndicatorObject(object _) { }
+    }
+}
